51811e1c40c2be9092edadda17cdf82d1efa1931
Fernando <00017917@uca.edu.sv>
2018-12-01 14:29:13 -0600

    primer commit, problemas de coneccion express

---
+++ config/database.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const {mongodb} = require('../config/keys');
+
+mongoose.connect(mongodb.URI,{
+
+    useCreateIndex: true,
+    useNewUrlParser: true
+})
+.then(db=>console.log('Coneccion hecha'))
+.catch(err=>console.error(err));
