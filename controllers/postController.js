0f1ff30109c08bf09902484a9547a749afec529a
Fernando <00017917@uca.edu.sv>
2018-12-01 17:29:09 -0600

    se termino el html y el ultimo controller, ya se agregaron a las rutas, pero sigue sin servir el maldito schema, por lo tanto no puedo hacer nada

--- controllers/postController.js
+++ controllers/postController.js
@@ -4,9 +4,10 @@ var controller = {};
 
 controller.create = function (req, res) {
     let data = {
-        materia: req.params.materia,
-        uv: req.params.uv,
-        descripcion: req.params.descripcion
+        id: req.params.id,
+        franquicia: req.params.franq,
+        nombre: req.params.nombre,
+        gas: req.params.precio
     }
 
     if (data.materia && data.uv && data.descripcion && data.materia != '' && data.uv != '' && data.descripcion != '') {
@@ -38,6 +39,17 @@ controller.get = function(req, res){
     })
 }
 
+controller.getAll = function(req, res){
+    postModel.findOne({}, function(err, posts){
+        if (err) {
+            res.status(500);
+            res.json({ code: 500, err });
+        } else {
+            res.json({ok: true, message: 'aca estan los posts', posts})
+        }
+    })
+}
+
 controller.remove = function(req, res){
     postModel.FindByIdAndRemove(req.params.id, function(err, eliminado){
         if (err) {
@@ -45,6 +57,25 @@ controller.remove = function(req, res){
             res.json({ code: 500, err });
         } else {
             res.json({ok: true, message: 'se elimino correctamente', eliminado})
+        }
+    })
+}
+
+controller.update = function(req, res){
+
+    let data = {
+        id: req.params.id,
+        franquicia: req.params.franq,
+        nombre: req.params.nombre,
+        gas: req.params.precio
+    }
+
+    postModel.findByIdAndUpdate(req.params.id, old, function(err, data){
+        if (err) {
+            res.status(500);
+            res.json({ code: 500, err });
+        } else {
+            res.json({ok: true, message: 'se actualizo correctamente', data})
         }
     })
 }
--- routes/posts.js
+++ routes/posts.js
@@ -2,6 +2,10 @@ var express = require('express'),
     router = express.Router(),
     controller = require('../controllers/postController');
 
+
+router.get('/', controller.getAll);
+
+
 router.get('/:id', controller.get);
 
 router.delete('/:id', controller.remove);
